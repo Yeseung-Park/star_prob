c202e7c0a68ae9a88c82c542672a9fed75329b6e
Yeseung-Park <yspark8538@naver.com>
2024-09-09 08:50:15 +0900

    이건 또 뭐지

--- 2447.py
+++ 2447.py
@@ -1,19 +1,18 @@
-# N = int(input())
+N = int(input())
 
-# def make_star(shape, n):    # shape: 기준이 되는 모양, n은 시행하는 횟수
-#     if n == int(N**(1/3)):
-#         return shape
-#     else:
-#         new_shape = shape*3+'\n'+f'{shape}'+' '*(3**n)+f'{shape}'+'\n'+shape*3
-#         n += 1
-#         return make_star(new_shape, n)
+def make_star(shape, n):    # shape: 기준이 되는 모양, n은 시행하는 횟수
+    if n == int(N**(1/3)):
+        return shape
+    else:
+        new_shape = [shape*3, shape*1+[' ']*(3**n)+shape*1, shape*3]
+        n += 1
+        return make_star(new_shape, n)
 
-# temp = 'hi'+'hi'
-# result = make_star('*', 0)
-# print(result)
-# print(temp)
+result = make_star('*', 0)
+for s in result:
+    print(result)
 
-temp = 'hi\nhi'
-print(temp)
-
-print(temp*2)
+# shape = ['*'*3, '*'*1+' '*1+'*'*1, '*'*3]
+#
+# for s in shape:
+#     print(s)
